e50b0eb422ecbf87d58c36e68dd2d259dfc887aa
Adrian Torres <adriantorres.gc@gmail.com>
2024-03-20 19:59:26 +0000

    modified:   src/js/views/Contact.js

--- src/js/views/Contact.js
+++ src/js/views/Contact.js
@@ -5,6 +5,8 @@ export const Contact = () => {
     <div className="container">
       <h1 className="text-center">Add a new contact</h1>
       <form>
+
+
         <div className="mb-3">
           <label for="fullName" className="form-label">
             Full Name
@@ -14,31 +16,49 @@ export const Contact = () => {
             className="form-control"
             id="textName"
             aria-describedby="FullName"
+            placeholder="Pon aqui tu Nombre completo"
           />
-          <div id="emailHelp" className="form-text">
-           
-          </div>
         </div>
+
+
         <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">
-            Password
+          <label for="InputEmail" className="form-label">
+            Email
           </label>
           <input
-            type="text"
+            type="email"
             className="form-control"
-            id="exampleInputPassword1"
+            id="InputEmail"
+            placeholder="Pon aqui tu Email"
           />
         </div>
-        <div className="mb-3 form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="exampleCheck1"
-          />
-          <label className="form-check-label" for="exampleCheck1">
-            Check me out
-          </label>
-        </div>
+
+
+<div className="mb-3">
+  <label for="InputTelefono" className="form-label">
+  Telefono
+  </label>
+  <input
+    type="text"
+    className="form-control"
+    id="InputTelefono"
+    placeholder="Pon aqui tu numero de Telefono"
+  />
+</div>
+
+
+<div className="mb-3">
+  <label for="InputDireccion" className="form-label">
+  Direccion
+  </label>
+  <input
+    type="text"
+    className="form-control"
+    id="InputDireccion"
+    placeholder="Pon aqui tu Direccion"
+  />
+</div>
+       
         <div className="d-grid ">
              <button className="btn btn-primary" type="button">Button</button>
   
